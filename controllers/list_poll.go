986b5eff3a22d04835d28630b7998ff6454f4989
Fayata <daffa12k@gmail.com>
2024-03-28 16:31:22 +0700

    get all polling data

--- controllers/list_poll.go
+++ controllers/list_poll.go
@@ -5,32 +5,33 @@ import (
 	"net/http"
 	"api-polling/routes"
 	"github.com/labstack/echo"
+	"log"
 )
 
 func AllList(e echo.Context) error {
-	var PollList []*models.Polling
+    var PollList []*models.Polling
 
-	db, err := routes.Conn()
-	if err != nil {
-		return err
-	}
-	defer db.Close()
+    db, err := routes.Conn()
+    if err != nil {
+        log.Println("Failed to connect to database:", err)
+        return err
+    }
+    defer db.Close()
 
-	rows, err := db.Query("SELECT poll_id, title, item1, item2 FROM polling")
-	if err != nil {
-		return err
-	}
-	defer rows.Close()
+    rows, err := db.Query("SELECT poll_id, title, item1, item2 FROM polling")
+    if err != nil {
+        log.Println("Failed to execute query:", err)
+        return err
+    }
+    defer rows.Close()
 
-	for rows.Next() {
-		var polling models.Polling
-		if err := rows.Scan(&polling.Poll_id, &polling.Title, &polling.Item1, &polling.Item2); err != nil {
-			return err
-		}
-		PollList = append(PollList, &polling)
-	}
-	return e.JSON(http.StatusOK, PollList)
-
-	
-
+    for rows.Next() {
+        var polling models.Polling
+        if err := rows.Scan(&polling.Poll_id, &polling.Title, &polling.Item1, &polling.Item2); err != nil {
+            log.Println("Failed to scan row:", err)
+            return err
+        }
+        PollList = append(PollList, &polling)
+    }
+    return e.JSON(http.StatusOK, PollList)
 }
--- routes/routes.go
+++ routes/routes.go
@@ -1,7 +1,9 @@
 package routes
 
-import "database/sql"
-
+import (
+	"database/sql"
+	_"github.com/go-sql-driver/mysql"
+)
 func Conn() (*sql.DB, error) {
 	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/db_api")
 	if err != nil {
